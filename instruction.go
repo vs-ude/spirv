9b717a4b1b25edca748a4289d2db15c9345a5008
FichteFoll <fichtefoll2@googlemail.com>
2019-11-20 00:12:00 +0100

    Implement functions to compare and set ResultIds
    Also add tests for all functions in instruction.go.

--- instruction.go
+++ instruction.go
@@ -37,9 +37,33 @@ func InstructionResultId(i Instruction) (Id, bool) {
 	return id, true
 }
 
+// InstructionResultId sets the value instruction's result Id,
+// provided it defines one.
+func SetInstructionResultId(i Instruction, id Id) bool {
+	rv := reflect.ValueOf(i)
+	rv = reflect.Indirect(rv)
+
+	field := rv.FieldByName("ResultId")
+	if field.Kind() == reflect.Invalid || !field.CanSet() {
+		return false
+	}
+	field.SetUint(uint64(id))
+	return true
+}
+
 // InstructionName returns the name for the given instruction.
 // This is the type name, minus some package cruft.
 func InstructionName(i Instruction) string {
 	name := fmt.Sprintf("%T", i)
 	return name[len("*spirv."):]
+}
+
+// InstructionEquals compares two instructions for equality.
+func InstructionEquals(i1 Instruction, i2 Instruction, withResultId bool) bool {
+	if !withResultId {
+		if id, ok := InstructionResultId(i1); ok {
+			SetInstructionResultId(i2, id)
+		}
+	}
+	return reflect.DeepEqual(i1, i2)
 }
